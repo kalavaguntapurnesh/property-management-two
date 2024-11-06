c527c2d645a1ba0c87fbb7769daee22166fbc286
@Purnesh1226 <purnesh.kalavagunta@clouddatanetworks.com>
2024-11-06 08:33:56 +0530

    First

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,12 +1,9 @@
 const Spinner = () => {
   return (
-    // <div className="flex items-center justify-center z-9999 min-h-screen">
-    //   <div className="flex justify-center items-center h-screen">
-    //     <span class="loader"></span>
-    //   </div>
-    // </div>
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    <div className="flex items-center justify-center bg-black z-50 min-h-screen fixed inset-0 bg-opacity-70">
+      <div className="flex justify-center items-center h-screen">
+        <span class="loader"></span>
+      </div>
     </div>
   );
 };
